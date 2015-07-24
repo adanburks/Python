4c6710e2e5f3982935bfd9fa5bba642f10f7eed1
adanburks <sburks@san.rr.com>
2015-07-24 15:33:52 -0700

    updates from work pc

--- LearnPythonTheHardWay/ex15.py
+++ LearnPythonTheHardWay/ex15.py
@@ -8,8 +8,11 @@ print "Here's your file %r:" % filename
 print txt.read()
 
 print "Type the filename again:"
-file_again = raw_input(">")
+file_again = raw_input("> ")
 
 txt_again = open(file_again)
 
 print txt_again.read()
+
+txt.close()
+txt_again.close()
